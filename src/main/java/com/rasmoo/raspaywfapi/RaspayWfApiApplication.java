686e2cd9ae11c06ee6c012abc27ca669d3d3001c
roberto <rvneto.rs@gmail.com>
2024-05-27 19:02:56 -0300

    Commit inicial

---
+++ src/main/java/com/rasmoo/raspaywfapi/RaspayWfApiApplication.java
@@ -0,0 +1,13 @@
+package com.rasmoo.raspaywfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspayWfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspayWfApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rasmoo/raspaywfapi/RaspayWfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasmoo.raspaywfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspayWfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
