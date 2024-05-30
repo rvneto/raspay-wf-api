67105856df6b59a4766f0f7b80b6c63e292ce432
roberto <rvneto.rs@gmail.com>
2024-05-30 16:21:46 -0300

    Aplicando a regra do pedido - II

--- src/main/java/com/rasmoo/raspaywfapi/dto/OrderDto.java
+++ src/main/java/com/rasmoo/raspaywfapi/dto/OrderDto.java
@@ -1,10 +1,18 @@
 package com.rasmoo.raspaywfapi.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 import java.math.BigDecimal;
 
 public record OrderDto(
+        @NotBlank
         String customerId,
+
+        @Min(0)
         BigDecimal discount,
+
+        @NotBlank
         String productAcronym
 ) {
 }
--- src/main/java/com/rasmoo/raspaywfapi/service/impl/OrderServiceImpl.java
+++ src/main/java/com/rasmoo/raspaywfapi/service/impl/OrderServiceImpl.java
@@ -1,10 +1,14 @@
 package com.rasmoo.raspaywfapi.service.impl;
 
 import com.rasmoo.raspaywfapi.dto.OrderDto;
+import com.rasmoo.raspaywfapi.exception.BadRequestException;
 import com.rasmoo.raspaywfapi.exception.NotFoundException;
+import com.rasmoo.raspaywfapi.mapper.OrderMapper;
 import com.rasmoo.raspaywfapi.model.Order;
 import com.rasmoo.raspaywfapi.repository.OrderRepository;
+import com.rasmoo.raspaywfapi.service.CustomerService;
 import com.rasmoo.raspaywfapi.service.OrderService;
+import com.rasmoo.raspaywfapi.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -13,7 +17,10 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+    private final OrderMapper mapper;
     private final OrderRepository repository;
+    private final CustomerService customerService;
+    private final ProductService productService;
 
     @Override
     public Mono<Order> findById(String id) {
@@ -23,7 +30,20 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Mono<Order> create(OrderDto dto) {
-        return null;
+        return customerService.findById(dto.customerId())
+                .flatMap(customer -> productService.findByAcronym(dto.productAcronym())
+                        .flatMap(product -> {
+                            Order order = mapper.toModel(dto);
+                            if (dto.discount().intValue() > 0) {
+                                if (dto.discount().compareTo(product.getCurrentPrice()) > 0) {
+                                    return Mono.error(() -> new BadRequestException("Discount can not be greater than currentPrice"));
+                                }
+                                order.setOriginalPrice(product.getCurrentPrice().subtract(dto.discount()));
+                            }
+                            order.setProduct(product);
+                            order.setCustomer(customer);
+                            return repository.save(order);
+                        }));
     }
 
 }
